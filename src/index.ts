dc21f25f8f16d49a488cc93be3fcd493f450e049
William Thorsen <wthorsen@skypilot.dev>
2019-11-21 12:37:28 -0700

    feat: Define common useful types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export {}
+/* Pseudo-types */
+export type Integer = number;
+
+export type Json = string;
+
+export type Timestamp = Integer;
+
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface JsonArray extends Array<JsonValue> {}
+
+export interface JsonObject { [key: string]: JsonValue }
+
+export type JsonValue = Literal | JsonObject | JsonArray | null
+
+export type Literal = boolean | number | string;
