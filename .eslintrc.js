3e73b9a307e3ee1e03a9910d4e1d2e0f90812861
William Thorsen <wthorsen@skypilot.dev>
2019-11-21 12:13:40 -0700

    Install & initialize Toolchain
    `yarn add --dev @skypilot/toolchain && yarn toolchain init`

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    './node_modules/@skypilot/toolchain/lib/configs/eslint.js',
+  ],
+};
---
+++ babel.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: './node_modules/@skypilot/toolchain/lib/configs/babel.js',
+};
